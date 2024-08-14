2db8b023a9ecf038a88187c423432b79b79a6197
RatnaTeja9 <ratnateja.ds@gmail.com>
2024-08-14 19:14:56 +0530

    Making code more redable

--- src/LikeButton.jsx
+++ src/LikeButton.jsx
@@ -3,24 +3,21 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import IconButton from "@mui/material/IconButton";
 import { useState } from "react";
 
-export default function LikeButton({}) {
-  const [isLike, setLike] = useState(false);
+export default function LikeButton() {
+  const [isLiked, setLiked] = useState(false);
 
-  function changeLike() {
-    if (isLike === false) {
-      setLike(true);
-    } else {
-      setLike(false);
-    }
+  function toggleLike() {
+    setLiked((prevLiked) => !prevLiked);
   }
+
   return (
     <>
-      <h1>Hit the like Button</h1>
-      <IconButton aria-label="like" onClick={changeLike}>
-        {isLike === false ? (
-          <FavoriteBorderIcon sx={{ fontSize: 50, color: "white" }} />
-        ) : (
+      <h1>Hit the Like Button</h1>
+      <IconButton aria-label="like" onClick={toggleLike}>
+        {isLiked ? (
           <FavoriteIcon sx={{ fontSize: 50, color: "red" }} />
+        ) : (
+          <FavoriteBorderIcon sx={{ fontSize: 50, color: "white" }} />
         )}
       </IconButton>
     </>
